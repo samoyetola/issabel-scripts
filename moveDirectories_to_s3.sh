144224cb1052058c445dd429c4a2644d289e2f30
Samuel Oyetola <osoyetola@gmail.com>
2023-09-07 10:44:01 +0100

    addfile1

---
+++ moveDirectories_to_s3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# S3 bucket name and destination path
+S3_BUCKET="callcenterarchive"
+S3_DESTINATION_PATH="test/"
+# Local directory to be copied
+LOCAL_DIR="/home/Unlimited/"
+# Copy the entire directory to S3
+ aws s3 cp "$LOCAL_DIR" "s3://$S3_BUCKET/$S3_DESTINATION_PATH" --recursive
+# Check if the copy was successful
+if [ $? -eq 0 ]; then
+    echo "Directory $LOCAL_DIR copied to S3 successfully."
+    # Delete the local directory and its contents after successful copy
+    sudo rm -r "$LOCAL_DIR"
+    echo "Directory $LOCAL_DIR deleted from the local file system."
+else
+    echo "Error copying directory $LOCAL_DIR to S3."
+fi
+echo "Script execution completed."
